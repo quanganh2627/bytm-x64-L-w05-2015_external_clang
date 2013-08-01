2f054b53ad7979ba9ed7dd234803638f49b6f8de
Yong Chen <yong.a.chen@intel.com>
2013-08-01 20:40:17 +0800

    Update clang for PVR DDK1.11
    BZ: 128073
    
    It is part of JB MR1 llvm and moved to clang during JB MR2 rebase.
    
    Change-Id: If35047a3e4bfa0450565f3df79fcb839bb45fc52
    Signed-off-by: Yong Chen <yong.a.chen@intel.com>
    Reviewed-on: http://android.intel.com:8080/123175
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Mao, Marc <marc.mao@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -4023,6 +4023,7 @@ llvm::Triple::ArchType darwin::getArchTypeForDarwinArchName(StringRef Str) {
     .Case("nvptx64", llvm::Triple::nvptx64)
     .Case("amdil", llvm::Triple::amdil)
     .Case("spir", llvm::Triple::spir)
+    .Case("usc", llvm::Triple::usc)
     .Default(llvm::Triple::UnknownArch);
 }
 
